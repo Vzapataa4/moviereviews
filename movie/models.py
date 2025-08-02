92314fe6566365d0094f837c4b8959b6d12ecde0
Vzapataa4 <valezapata10@gmail.com>
2025-08-02 13:06:06 -0500

    busqueda de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='movie/images/')  # 👈 este es el nuevo campo
+
+    def __str__(self):
+        return self.title
+
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Movie
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Valentina Zapata'})
+    search_query = request.GET.get('searchMovie')
+    if search_query:
+        movies = Movie.objects.filter(title__icontains=search_query)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
